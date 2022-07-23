6ca1edc39197349dc2d751fbf0a83126cb73a87d
Caleb <computerguy10@gmail.com>
2022-07-23 06:22:02 -0400

    Added 100% Test Coverage

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -21,4 +21,24 @@ describe("SimpleStorage", function () {
         const currentValue = await simpleStorage.retrieve();
         assert.equal(currentValue.toString(), expectedValue);
     });
+    it("Should add a person with their favorite number", async function () {
+        const expectedNumber = "10";
+        const expectedName = "Caleb";
+        const transactionResponse = await simpleStorage.addPerson(
+            expectedName,
+            expectedNumber
+        );
+        await transactionResponse.wait(1);
+
+        const currentPerson = await simpleStorage.people(0);
+        const currentName = currentPerson[1];
+        const currentNumber = currentPerson[0];
+        const nameToNumber = await simpleStorage.nameToFavoriteNumber(
+            currentName
+        );
+
+        assert.strictEqual(currentName, expectedName);
+        assert.equal(currentNumber.toString(), expectedNumber);
+        assert.equal(nameToNumber.toString(), expectedNumber);
+    });
 });
